051db0903717420ae0b4105651fd186a1574ad8a
Adrien490 <adrien.poirier49@gmail.com>
2025-08-18 15:33:29 +0200

    fix:layout

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,17 +1,14 @@
 import { Footer } from "@/app/(marketing)/components/footer";
 import { Navbar } from "@/app/(marketing)/components/navbar";
-import { isAdmin } from "@/domains/user/utils/is-admin";
 
 export default async function PublicLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
-	const userIsAdmin = await isAdmin();
-
 	return (
 		<>
-			<Navbar isAdmin={userIsAdmin} />
+			<Navbar />
 			<main role="main" className="min-h-screen bg-background">
 				{children}
 			</main>
